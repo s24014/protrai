a92bb2764aae76427fa7b985d5b94f8adb257ac3
島袋 時任 <s24014@std.it-college.ac.jp>
2024-06-03 07:53:15 +0900

    fist commit

---
+++ kuji.py
@@ -0,0 +1,3 @@
+import randam
+kuji = ["大吉","中吉","小吉","凶","大凶"]
+print(randam.choice(kuji))
---
+++ 新しいフォルダー/bmi.py
@@ -0,0 +1,7 @@
+#s24014
+#BMI値計算プログラム
+h = float(input("身長何cmですか？"))/ 100.0
+w = float(input("体重何kgですか？"))
+
+bmi = w(h + h)
+print("あなたのBMI値は",bmi,"です。")
