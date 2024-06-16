1ffdd3eb22fb9684969cb685e25ad954a11e8204
島袋 時任 <s24014@std.it-college.ac.jp>
2024-06-17 05:27:38 +0900

    add def01.py

---
+++ def01.py
@@ -0,0 +1,7 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/島袋時任/protrai/def1.py
+こんにちは
+こんにちは
+こんにちは
